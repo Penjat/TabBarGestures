1c552b4689a52bf50b34d7754c4ea5e89e5c045b
Penjat <theSpencerSymington@Gmail.com>
2019-01-17 17:07:44 -0800

    Initial Commit

---
+++ TabBarGestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabBarGestures
+//
+//  Created by Spencer Symington on 2019-01-17.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabBarGestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabBarGestures
+//
+//  Created by Spencer Symington on 2019-01-17.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
